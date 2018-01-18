c340fadb12d93416fa7877936332460377c1e86f
dianlujitao <dianlujitao@lineageos.org>
2018-01-18 21:34:00 +0800

    interfaces: power: Add HIDL HAL interface
     * Introduce necessary API and hint IDs for power profile support.
    
    Change-Id: Ie50f36e6019dc4dfd7fc6314e3dfd2977c05d718

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.lineage:hardware/arrow/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
