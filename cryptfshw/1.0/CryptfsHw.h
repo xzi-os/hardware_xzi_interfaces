0586b5136f7f5f6690f4c89d92c9d638ec70e7e1
kubersharma001 <kubersharma001@gmail.com>
2019-12-05 19:20:44 +0530

    cryptfshw: Revert back to Initial dummy implementation
    Revert "cryptfshw: dlsym: Reverse wait for qsee flag"
    This reverts commit 7cbf025224667d6fc05aad7ad7d9f8c04a81b6f9.
    
    Revert "cryptfshw: dlsym: Wire up the implementation"
    This reverts commit ff3510aff77fdb9de70b050114c18d6d8dc94650.
    
    Revert "cryptfshw: ioctl: Wire up the implementation"
    This reverts commit e005a4ac40ed5eacff47525e8cf722c3f38c7b24.
    
    Revert "cryptfshw: Split implementations into dlsym and ioctl"
    This reverts commit 866dc31acd23d5e749a553db61a869d36401f4ab.
    
    Revert "cryptfshw: Setup boilerplate items before actual implementation"
    This reverts commit 3bdcf55d7ed0398a8d6ec122080e10199824adf4.
    
    Change-Id: Icd2b048068c197073523cafd2aae74e82dd163d5

--- cryptfshw/1.0/CryptfsHw.h
+++ cryptfshw/1.0/CryptfsHw.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,59 +16,40 @@
 
 #pragma once
 
+#include <vendor/qti/hardware/cryptfshw/1.0/ICryptfsHw.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
-#include <vendor/qti/hardware/cryptfshw/1.0/ICryptfsHw.h>
 
 namespace vendor {
 namespace qti {
 namespace hardware {
 namespace cryptfshw {
 namespace V1_0 {
-namespace ioctl_qti {
+namespace implementation {
 
-using ::android::sp;
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
 using ::android::hardware::hidl_string;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
+using ::android::sp;
 
-#define QTI_ICE_STORAGE_NOT -1
-#define QTI_ICE_STORAGE_UFS 1
-#define QTI_ICE_STORAGE_SDCC 2
-
-#define CRYPTFS_HW_KMS_WIPE_KEY 1
-#define CRYPTFS_HW_CREATE_KEY_FAILED -7
-#define CRYPTFS_HW_WIPE_KEY_FAILED -8
-#define CRYPTFS_HW_UPDATE_KEY_FAILED -9
-#define CRYPTFS_HW_KMS_MAX_FAILURE -10
-
-enum cryptfs_hw_key_management_usage_type {
-    CRYPTFS_HW_KM_USAGE_DISK_ENCRYPTION = 0x01,
-    CRYPTFS_HW_KM_USAGE_FILE_ENCRYPTION = 0x02,
-    CRYPTFS_HW_KM_USAGE_UFS_ICE_DISK_ENCRYPTION = 0x03,
-    CRYPTFS_HW_KM_USAGE_SDCC_ICE_DISK_ENCRYPTION = 0x04,
-    CRYPTFS_HW_KM_USAGE_MAX
-};
-
-class CryptfsHw : public ICryptfsHw {
-   public:
-    CryptfsHw();
-
+struct CryptfsHw : public ICryptfsHw {
     // Methods from ::vendor::qti::hardware::cryptfshw::V1_0::ICryptfsHw follow.
     Return<int32_t> setIceParam(uint32_t flag) override;
-    Return<int32_t> setKey(const hidl_string& passwd, const hidl_string&) override;
-    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw,
-                              const hidl_string&) override;
+    Return<int32_t> setKey(const hidl_string& passwd, const hidl_string& enc_mode) override;
+    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw, const hidl_string& enc_mode) override;
     Return<int32_t> clearKey() override;
 
-   private:
-    int mStorageType;
+    // Methods from ::android::hidl::base::V1_0::IBase follow.
+
 };
 
-}  // namespace ioctl_qti
+// FIXME: most likely delete, this is only for passthrough implementations
+// extern "C" ICryptfsHw* HIDL_FETCH_ICryptfsHw(const char* name);
+
+}  // namespace implementation
 }  // namespace V1_0
 }  // namespace cryptfshw
 }  // namespace hardware
