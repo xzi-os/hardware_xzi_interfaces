95214212284fb623c946d6adea26c5272b26b3c2
Mikazuuu07 <irfn4566@gmail.com>
2022-03-21 16:17:28 +0000

    Adapt to Xzi
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.arrow:hardware/arrow/interfaces" \
+  "vendor.xzi:hardware/xzi/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
